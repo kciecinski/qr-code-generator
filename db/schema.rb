bc276eef07636cf298f19e6ebdfa67a937716f9e
kciecinski <ciecinski.kacper@gmail.com>
2020-11-17 22:29:00 +0100

    gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20201117203821) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tickets", force: :cascade do |t|
     t.datetime "date"
     t.string "first_name"
